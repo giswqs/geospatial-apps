45313d32768877241151118056bbdcec1fb2cef7
Qiusheng Wu <giswqs@gmail.com>
2022-03-14 11:01:10 -0400

    Added basemaps to scotland app

--- apps/scotland.py
+++ apps/scotland.py
@@ -8,8 +8,9 @@ def app():
 
     st.title('National Library of Scotland XYZ Layers')
     df = pd.read_csv('data/scotland_xyz.tsv', sep='\t')
-    names = df['Name'].values.tolist()
-    links = df['URL'].values.tolist()
+    basemaps = leafmap.folium_basemaps
+    names = df['Name'].values.tolist() + list(basemaps.keys())
+    links = df['URL'].values.tolist() + list(basemaps.values())
     
     col1, col2 = st.columns(2)
     with col1:
@@ -21,20 +22,27 @@ def app():
 
     m = leafmap.Map(center=[55.68,-2.98], zoom=6)
 
-    left_layer = folium.TileLayer(
-        tiles=links[names.index(left_name)],
-        name=left_name,
-        attr='National Library of Scotland',
-        overlay=True
-        
-    )
-    right_layer = folium.TileLayer(
-        tiles=links[names.index(right_name)],
-        name=right_name,
-        attr='National Library of Scotland',
-        overlay=True
+    if left_name in basemaps:
+        left_layer = basemaps[left_name]
+    else:
+        left_layer = folium.TileLayer(
+            tiles=links[names.index(left_name)],
+            name=left_name,
+            attr='National Library of Scotland',
+            overlay=True
+            
+        )
 
-    )
+    if right_name in basemaps:
+        right_layer = basemaps[right_name]
+    else:
+        right_layer = folium.TileLayer(
+            tiles=links[names.index(right_name)],
+            name=right_name,
+            attr='National Library of Scotland',
+            overlay=True
+
+        )
 
     m.split_map(left_layer, right_layer)
     m.to_streamlit(height=800)
