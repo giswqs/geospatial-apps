2df5b5521c13b7e3b17ef409fc0c56d9a1df694b
Qiusheng Wu <giswqs@gmail.com>
2022-07-22 15:32:29 -0400

    Updated split map

--- apps/scotland.py
+++ apps/scotland.py
@@ -15,7 +15,7 @@ def app():
     col1, col2, col3, col4, col5, col6 = st.columns([3, 3, 1, 1, 1, 1.5])
     with col1:
         left_name = st.selectbox(
-            'Select the left layer', names, index=names.index('Great Britain - OS 1:25,000, 1937-61')
+            'Select the left layer', names, index=names.index('Great Britain - Bartholomew Half Inch, 1897-1907')
         )
 
     with col2:
@@ -39,6 +39,17 @@ def app():
 
     with col6:
         checkbox = st.checkbox('Add OS 25 inch')
+
+    # with col7:
+    with st.expander("Acknowledgements"):
+        markdown = """
+        The map tile access is by kind arrangement of the National Library of Scotland on the understanding that re-use is for personal purposes. They host most of the map layers except these:
+    - The Roy Maps are owned by the British Library.
+    - The Great Britain – OS maps 1:25,000, 1937-61 and One Inch 7th series, 1955-61 are hosted by MapTiler.
+
+    If you wish you use these layers within a website, or for a commercial or public purpose, please view the [National Library of Scotland Historic Maps Subscription API](https://maps.nls.uk/projects/subscription-api/) or contact them at maps@nls.uk.
+        """
+        st.markdown(markdown, unsafe_allow_html=True)
 
     m = leafmap.Map(
         center=[float(lat), float(lon)],
